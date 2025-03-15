b0badf939c29861a0673b0e342418b3f5bd8f140
ZX夏夜之风 <snwcreations@qq.com>
2025-03-15 13:12:03 +0800

    feat: add IGamePlayer getters to IRFMPlugin

--- src/main/java/snw/rfm/api/IRFMPlugin.java
+++ src/main/java/snw/rfm/api/IRFMPlugin.java
@@ -1,7 +1,15 @@
 package snw.rfm.api;
 
+import org.bukkit.entity.Player;
 import org.bukkit.plugin.Plugin;
+import snw.rfm.api.game.IGamePlayer;
+
+import java.util.UUID;
 
 public interface IRFMPlugin extends Plugin {
     boolean isDisabling();
+
+    IGamePlayer getGamePlayer(Player player);
+
+    IGamePlayer getGamePlayer(UUID uuid);
 }
