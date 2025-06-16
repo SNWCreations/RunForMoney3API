e93f94d6be57fc0780b41f8fd343523a081eedd7
ZX夏夜之风 <snwcreations@qq.com>
2025-06-16 13:40:28 +0800

    feat: expose the group getter to IGamePlayer

--- common/src/main/java/snw/rfm/api/game/IGamePlayer.java
+++ common/src/main/java/snw/rfm/api/game/IGamePlayer.java
@@ -1,6 +1,7 @@
 package snw.rfm.api.game;
 
 import org.jetbrains.annotations.ApiStatus;
+import snw.rfm.api.game.teaming.GamePlayerGroup;
 
 import java.util.Optional;
 import java.util.UUID;
@@ -12,6 +13,8 @@ public interface IGamePlayer {
     Optional<? extends IGame> getGame();
 
     Optional<? extends IGameTeam> getTeam();
+
+    Optional<? extends GamePlayerGroup> getGroup();
 
     Optional<String> getName();
 
