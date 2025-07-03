8af651c5d9d8ab8e4739db66ca7056febe88d3a5
ZX夏夜之风 <snwcreations@qq.com>
2025-07-03 13:28:17 +0800

    feat: add ILevel#getKey

--- bukkit/src/main/java/snw/rfm/api/abc/BukkitLevel.java
+++ bukkit/src/main/java/snw/rfm/api/abc/BukkitLevel.java
@@ -3,4 +3,8 @@ package snw.rfm.api.abc;
 import org.bukkit.World;
 
 public record BukkitLevel(World handle) implements ILevel {
+    @Override
+    public String getKey() {
+        return handle.getKey().toString();
+    }
 }
--- common/src/main/java/snw/rfm/api/abc/ILevel.java
+++ common/src/main/java/snw/rfm/api/abc/ILevel.java
@@ -1,4 +1,5 @@
 package snw.rfm.api.abc;
 
 public interface ILevel {
+    String getKey();
 }
